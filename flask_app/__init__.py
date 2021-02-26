cfdab38c2b8dc6598ae4dc724b22e7d8844517f9
anthgrif <32582804+anthgrif@users.noreply.github.com>
2021-02-26 13:30:02 -0800

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -16,67 +16,64 @@ app.mongo_client = MongoClient(app.config['MONGODB_URI'])
 db = app.mongo_client.harvard_db
 
 
-try:
-    collection = db.data
+collection = db.data
 
-    collection.drop()
-    
-    # app.elasticsearch.indices.delete('harvard', ignore=[400, 404])
+collection.drop()
 
-    # Load File
-    with open('flask_app/harvard_dataverse.json') as raw_file:
-        raw = json.load(raw_file)
+# app.elasticsearch.indices.delete('harvard', ignore=[400, 404])
 
-    # Loads documents from JSON file into collection called 'data'
-    collection.insert(raw)
+# Load File
+with open('flask_app/harvard_dataverse.json') as raw_file:
+    raw = json.load(raw_file)
 
-    #Finds all documents, assigns cursor to res
-    res = collection.find()
+# Loads documents from JSON file into collection called 'data'
+collection.insert(raw)
 
-    # Checks how many documents have been loaded into MongoDB
-    num_docs = collection.estimated_document_count()
+#Finds all documents, assigns cursor to res
+res = collection.find()
 
-    # # Pull from Mongo and dump into ES w/ bulk indexing
-    # actions = []
-    # for i in range(num_docs):
-    #     doc = res[i]
+# Checks how many documents have been loaded into MongoDB
+num_docs = collection.estimated_document_count()
 
-    #     # Remove ID from MongoDB entry, so no duplicate IDs are given in elastic search index
-    #     doc.pop('_id')
+# # Pull from Mongo and dump into ES w/ bulk indexing
+# actions = []
+# for i in range(num_docs):
+#     doc = res[i]
 
-    #     action = {
-    #         "_index": 'harvard',
-    #         "_source": json.dumps(doc)
-    #     }
-    #     actions.append(action)
+#     # Remove ID from MongoDB entry, so no duplicate IDs are given in elastic search index
+#     doc.pop('_id')
 
-    # # Mapping that matches requisites provided by Dr. Wu
-    # custom_map = {
-    #     "settings": {
-    #         "analysis": {
-    #             "normalizer": {
-    #                 "case_insensitive": {
-    #                     "type": "custom",
-    #                     "filter": ["lowercase"]
-    #                 }
-    #             }
-    #         }
-    #     },
-    #     "mappings": {
-    #         "properties": {
-    #             "funder.name": {
-    #                 "type": "keyword",
-    #                 "normalizer": "case_insensitive"		    }
-                                                
-    #         }
-    #     }
-    # }
-    
-    # app.elasticsearch.indices.create(index='harvard', body=custom_map)
-    # helpers.bulk(app.elasticsearch, actions, request_timeout=30)
+#     action = {
+#         "_index": 'harvard',
+#         "_source": json.dumps(doc)
+#     }
+#     actions.append(action)
+
+# # Mapping that matches requisites provided by Dr. Wu
+# custom_map = {
+#     "settings": {
+#         "analysis": {
+#             "normalizer": {
+#                 "case_insensitive": {
+#                     "type": "custom",
+#                     "filter": ["lowercase"]
+#                 }
+#             }
+#         }
+#     },
+#     "mappings": {
+#         "properties": {
+#             "funder.name": {
+#                 "type": "keyword",
+#                 "normalizer": "case_insensitive"		    }
+                                            
+#         }
+#     }
+# }
+
+# app.elasticsearch.indices.create(index='harvard', body=custom_map)
+# helpers.bulk(app.elasticsearch, actions, request_timeout=30)
         
-except:
-    print("Unexpected Error: ", sys.exc_info())
 
 # Workaround for the circular import problem
 from flask_app import routes
